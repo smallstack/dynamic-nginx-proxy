a72f11f386a1fe25a0b6a515a4da8a5807d9a282
Maximilian Friedmann <max@smallstack.io>
2018-02-01 17:19:44 +0100

    only replacing PROXY_PASS

--- replace_env_and_run.sh
+++ replace_env_and_run.sh
@@ -1,7 +1,7 @@
 set -e
 
 echo "replacing environment variables in nginx.conf..."
-envsubst < /opt/work/nginx.conf > /etc/nginx/nginx.conf
+envsubst '\$PROXY_PASS' < /opt/work/nginx.conf > /etc/nginx/nginx.conf
 
 echo "Starting nginx with command: nginx -g 'daemon off;'"
 nginx -g 'daemon off;'
